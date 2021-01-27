d3282c478ba52f031fcb7e0da4cd5d22900c93f9
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2021-01-27 17:12:46 +0400

    feat: add Variants of Home Task N1 from Netology

---
+++ NetologyVariantHomeWork1.py
@@ -0,0 +1,30 @@
+# # Task N2
+
+# date_1 = input('Введите дату: ')
+# task_1 = input('Введите задачу: ')
+# date_2 = input('Введите дату: ')
+# task_2 = input('Введите задачу: ')
+# date_3 = input('Введите дату: ')
+# task_3 = input('Введите задачу: ')
+
+# print(date_1, task_1)
+# print(date_2, task_2)
+# print(date_3, task_3)
+
+
+# Task N3
+
+task_dict = {}
+
+date = input("Введите дату: ")
+task = input("Введите задачу: ")
+task_dict[date] = task
+
+date = input("Введите дату: ")
+task = input("Введите задачу: ")
+task_dict[date] = task
+
+
+date = input("Введите дату: ")
+task = input("Введите задачу: ")
+task_dict[date] = task
