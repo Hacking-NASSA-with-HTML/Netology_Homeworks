9f18eaefe2654f872f63a2383fe4f26630bda757
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2021-01-27 16:51:05 +0400

    init: Netology homeworks start

---
+++ homework1.py
@@ -0,0 +1,39 @@
+# # Task N1
+
+# inputed_date = input("Введите дату: ")
+# inputed_task = input("Введите задачу: ")
+
+# print(inputed_date + " " + inputed_task)
+
+
+# # Task N2
+
+# inputed_date1 = input("Введите дату: ")
+# inputed_task1 = input("Введите задачу: ")
+
+# inputed_date2 = input("Введите дату: ")
+# inputed_task2 = input("Введите задачу: ")
+
+# inputed_date3 = input("Введите дату: ")
+# inputed_task3 = input("Введите задачу: ")
+
+# print(inputed_date1 + " " + inputed_task1)
+# print(inputed_date2 + " " + inputed_task2)
+# print(inputed_date3 + " " + inputed_task3)
+
+
+# Task N3
+
+activity = {}
+
+inputed_date1 = input("Введите дату: ")
+inputed_task1 = input("Введите задачу: ")
+activity[inputed_date1] = inputed_task1
+
+inputed_date2 = input("Введите дату: ")
+inputed_task2 = input("Введите задачу: ")
+activity[inputed_date2] = inputed_task2
+
+inputed_date3 = input("Введите дату: ")
+inputed_task3 = input("Введите задачу: ")
+activity[inputed_date3] = inputed_task3
